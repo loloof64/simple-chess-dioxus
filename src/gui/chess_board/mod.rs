b5a90295ab2b510cd0db30c7cd518415df73ad11
laurent bernabé <laurent.bernabe@gmail.com>
2024-12-14 11:38:55 +0100

    draw chess board cells
    but without pieces nor coordinates

--- src/gui/chess_board/mod.rs
+++ src/gui/chess_board/mod.rs
@@ -1,5 +1,24 @@
 use dioxus::prelude::*;
 
+
+#[derive(Debug, Clone)]
+struct CellParams {
+    color: String,
+}
+
+#[component]
+fn Cell(params: ReadOnlySignal<CellParams>) -> Element {
+    let color = params.read().clone().color;
+    rsx! {
+        div {
+            width: "100%",
+            height: "100%",
+            background_color: color
+        }
+    }
+}
+
+
 #[derive(Debug, Clone)]
 pub struct ChessboardParams {
     pub position: String,
@@ -14,9 +33,21 @@ pub fn Chessboard(params: ReadOnlySignal<ChessboardParams>) -> Element {
 
     rsx!    {
         div {
-            background_color: "blue",
+            display: "grid",
+            grid_template: "repeat(8, 1fr) / repeat(8, 1fr)",
             width: size.as_str(),
             height: size.as_str(),
+
+            for row in (0..8) {
+                for col in (0..8) {
+                    Cell {
+                        params: CellParams {
+                            color: if (row + col) % 2 == 0 { "navajowhite".to_string() } else { "peru".to_string() },
+                        }
+                    }
+                }
+            }
         }
+            
     }
 }
